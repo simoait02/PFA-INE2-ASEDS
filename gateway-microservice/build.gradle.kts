49a825623d8033ca75613502f341d5437fef8365
simoait02 <simoaithssaine09@gmail.com>
2025-05-05 18:35:16 +0100

    add microservice gateway

--- gateway-microservice/build.gradle.kts
+++ gateway-microservice/build.gradle.kts
@@ -1,35 +1,28 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
-val springCloudVersion by extra("2024.0.1")
 
 group = "com.aseds"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
-	}
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- gateway-microservice/src/main/java/com/aseds/gatewaymicroservice/GatewayMicroserviceApplication.java
+++ gateway-microservice/src/main/java/com/aseds/gatewaymicroservice/GatewayMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMicroserviceApplication.class, args);
+    }
 
 }
