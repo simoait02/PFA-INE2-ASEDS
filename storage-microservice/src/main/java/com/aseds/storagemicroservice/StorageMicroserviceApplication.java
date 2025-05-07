7e64e5c82940bbe0367ccc10cd3aea1c1a5ec860
simoait02 <simoaithssaine09@gmail.com>
2025-05-07 15:32:19 +0100

    add storage microservice

---
+++ storage-microservice/src/main/java/com/aseds/storagemicroservice/StorageMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aseds.storagemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageMicroserviceApplication.class, args);
+    }
+
+}
---
+++ storage-microservice/src/test/java/com/aseds/storagemicroservice/StorageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aseds.storagemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
